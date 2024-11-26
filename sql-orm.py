8a8c7160134acde088e41ae71c16364249f34bb4
Lewis Wheeler <Lewisswheelerr@outlook.com>
2024-11-26 12:11:57 +0000

    Succesfully use ORM method to access chinook database using class based models

--- sql-orm.py
+++ sql-orm.py
@@ -19,11 +19,11 @@ class Album(base):
     __tablename__ = "Album"
     AlbumId = Column(Integer, primary_key=True)
     Title = Column(String)
-    ArtistId = Column(Integerm, ForeignKey("Artist.ArtistId"))
+    ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
 
 class Track(base):
     __tablename__ = "Track"
-    TrackId = Column(Integer, primarykey=True)
+    TrackId = Column(Integer, primary_key=True)
     Name = Column(String)
     AlbumId = Column(Integer, ForeignKey("Album.AlbumId"))
     MediaTypeId = Column(Integer, primary_key=False)
@@ -40,4 +40,42 @@ Session = sessionmaker(db)
 session = Session()
 
 # creating the databse using declarative_base subclass
-base.metadata.create.all(db)
+base.metadata.create_all(db)
+
+# Query 1 - select all records from the Artist Table
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 2 - select only the "Name" column from the Artist Table
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.Name)
+
+# Query 3 - select only "Queen" from the Artist Table
+# artist = session.query(Artist).filter_by(Name="Queen").first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 4 - select only by ArtistId #51 from the Artist Table
+# artist = session.query(Artist).filter_by(ArtistId=51).first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 5 - select only the albums with ArtistId #51 on the Album Table
+albums = session.query(Album).filter_by(ArtistId=51)
+for album in albums:
+    print(album.AlbumId, album.Title, album.ArtistId, sep=" | ")
+
+# Query 6 - select only the tracks of the composer "Queen" from the Track table
+tracks = session.query(Track).filter_by(Composer="Queen")
+for track in tracks:
+    print(
+        track.TrackId,
+        track.Name,
+        track.AlbumId,
+        track.MediaTypeId,
+        track.GenreId,
+        track.Composer,
+        track.Milliseconds,
+        track.Bytes,
+        track.UnitPrice,
+        sep=" | ")
