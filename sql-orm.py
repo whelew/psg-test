2485e5801474d68b8be8bae3555498e28aaaaa2d
Lewis Wheeler <Lewisswheelerr@outlook.com>
2024-11-26 11:54:30 +0000

    Created sql-orm.py file

---
+++ sql-orm.py
@@ -0,0 +1,43 @@
+from sqlalchemy import (
+    create_engine, Column, Float, ForeignKey, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# executing the instructions from the "chinook" db
+db = create_engine("postgresql:///chinook")
+# base will essentially grab the metadata produced by our db
+# creates subclass to mapeverything back to us
+base = declarative_base()
+
+class Artist(base):
+    __tablename__ = "Artist"
+    ArtistId = Column(Integer, primary_key=True)
+    Name = Column(String)
+
+class Album(base):
+    __tablename__ = "Album"
+    AlbumId = Column(Integer, primary_key=True)
+    Title = Column(String)
+    ArtistId = Column(Integerm, ForeignKey("Artist.ArtistId"))
+
+class Track(base):
+    __tablename__ = "Track"
+    TrackId = Column(Integer, primarykey=True)
+    Name = Column(String)
+    AlbumId = Column(Integer, ForeignKey("Album.AlbumId"))
+    MediaTypeId = Column(Integer, primary_key=False)
+    GenreId = Column(Integer, primary_key=False)
+    Composer = Column(String)
+    Milliseconds = Column(Integer, primary_key=False)
+    Bytes = Column(Integer, primary_key=False)
+    UnitPrice = Column(Float)
+
+# instead of connecting to the database directly we will ask for a session
+# create a new instance of sessionmaker, then point to our engine (the db)
+Session = sessionmaker(db)
+# opens an actual session by calling the Session() subclass defined above
+session = Session()
+
+# creating the databse using declarative_base subclass
+base.metadata.create.all(db)
--- sql.expression.py
+++ sql.expression.py
@@ -2,15 +2,62 @@ from sqlalchemy import (
     create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
 )
 
-#  executing the instructions from our localhost "chinook" db
-db = create_engine("postgresql///:chinook")
+# executing the instructions from our localhost "chinook" db
+db = create_engine("postgresql:///chinook")
 
 meta = MetaData(db)
 
 # create variable for "Artist" table
 artist_table = Table(
     "Artist", meta,
-    
+    Column("ArtistId", Integer, primary_key=True),
+    Column("Name", String)
 )
+
+# create variable for "Album" table
+album_table = Table(
+    "Album", meta,
+    Column("AlbumId", Integer, primary_key=True),
+    Column("Title", String),
+    Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
+)
+
+# create variable for "Artist" table
+track_table = Table(
+    "Track", meta,
+    Column("TrackId", Integer, primary_key=True),
+    Column("Name", String),
+    Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
+    Column("MediaTypeId", Integer, primary_key=False),
+    Column("GenreId", Integer, primary_key=False),
+    Column("Composer", String),
+    Column("Milliseconds", Integer),
+    Column("Bytes", Integer),
+    Column("UnitPrice", Float)
+)
+
+
 #  make connection
 with db.connect() as connection:
+
+    # Query 1 - select all artists from the "Artist" table
+    # select_query = artist_table.select()
+
+    # Query 2 - select only the "Name" column from the "Artist" table
+    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+
+    # query 3 - select only "Queen" from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+
+    # query 4 - select only "ArtistId" #51 from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+
+    # query 5 - select only "Queen" from the "Album" table
+    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
+
+    # query 6 - select all tracks from where the composer is "Queen" from the "Track" table
+    select_query = track_table.select().where(track_table.c.Composer == "Queen")
+    
+    results = connection.execute(select_query)
+    for result in results:
+        print(result)
